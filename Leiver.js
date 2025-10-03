da64418298d481ac3444a050dde8659c985ed183
LeiverRuben <leiver.zamora@est.ucacue.edu.ec>
2025-10-02 21:34:25 -0500

    Segundo commit

--- Leiver.js
+++ Leiver.js
@@ -2,3 +2,19 @@ function hello() {
     var mensaje = "hello leiver";
     console.log(mensaje);
 }
+function hello2() {
+    var mensaje = "hello leiver";
+    console.log(mensaje);
+}
+function hello3() {
+    var mensaje = "hello leiver";
+    console.log(mensaje);
+}
+function hello4() {
+    var mensaje = "hello leiver";
+    console.log(mensaje);
+}
+function hello5() {
+    var mensaje = "hello leiver";
+    console.log(mensaje);
+}
