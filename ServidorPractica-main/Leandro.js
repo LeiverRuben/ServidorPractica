6cc8f6a8f31bfcda5dfc52349cba6b14719e1c7b
Leandro <leandro.vega@est.ucacue.edu.ec>
2025-10-02 23:41:22 -0500

    Quinto commit

---
+++ ServidorPractica-main/Leandro.js
@@ -0,0 +1,24 @@
+function Hello() {
+    var mensaje = "hello leandro ";
+    console.log(mensaje);
+}
+
+function Hello2() {
+    var mensaje = "hello leandro 2";
+    console.log(mensaje);
+}
+
+function Hello3() {
+    var mensaje = "hello leandro 3";
+    console.log(mensaje);
+}
+
+function Hello4() {
+    var mensaje = "hello leandro 4";
+    console.log(mensaje);
+}
+
+function Hello5() {
+    var mensaje = "hello leandro 5";
+    console.log(mensaje);
+}
